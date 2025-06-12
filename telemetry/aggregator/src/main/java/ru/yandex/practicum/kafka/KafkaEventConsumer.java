df10f9b3c8e9b68ae0226b3f53d4387c25574c75
YZY4070 <muteallchat30@yandex.ru>
2025-06-12 06:24:07 +0300

    Исправил замечания

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/KafkaEventConsumer.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/KafkaEventConsumer.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
@@ -16,18 +17,17 @@ import java.util.Properties;
 public class KafkaEventConsumer implements AutoCloseable {
     private final KafkaConsumer<String, SpecificRecordBase> kafkaConsumer;
 
-    private Properties consumerProperties() {
-        Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregator-group");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class.getName());
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-        return props;
-    }
+    public KafkaEventConsumer(@Value("${kafka.bootstrap-servers}") String bootstrapServers,
+                                @Value("${kafka.group-id}") String groupId,
+                                @Value("${kafka.auto-commit}") String autoCommit) {
+        Properties config = new Properties();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, autoCommit);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class.getName());
 
-    public KafkaEventConsumer() {
-        this.kafkaConsumer = new KafkaConsumer<>(consumerProperties());
+        this.kafkaConsumer = new KafkaConsumer<>(config);
     }
 
     public ConsumerRecords<String, SpecificRecordBase> poll(Duration duration) {
--- telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/KafkaEventProducer.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/KafkaEventProducer.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import serializer.AvroSerializer;
 
@@ -15,16 +16,13 @@ import java.util.Properties;
 public class KafkaEventProducer implements AutoCloseable {
     private final KafkaProducer<String, SpecificRecordBase> kafkaProducer;
 
-    private Properties producerProperties() {
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class.getName());
-        return props;
-    }
+    public KafkaEventProducer(@Value("${kafka.bootstrap-servers}") String bootstrapServers) {
+        Properties config = new Properties();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class.getName());
 
-    public KafkaEventProducer() {
-        this.kafkaProducer = new KafkaProducer<>(producerProperties());
+        this.kafkaProducer = new KafkaProducer<>(config);
     }
 
     public void send(String topic, String key, SpecificRecordBase value) {
